d6d5d67fb088a5b5f024d02655e35db64c2e089a
eetukarttunen <eetu.karttunen@outlook.com>
2025-08-06 10:15:15 +0300

    style: added ASCII art

---
+++ cli-ts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Mandatory epic ascii art :)
+cat << "EOF"
+
+  ____ _     ___   _____ _                     _               _
+ / ___| |   |_ _| |_   _(_)_ __ ___   ___  ___| |__   ___  ___| |_
+| |   | |    | |    | | | | '_ ` _ \ / _ \/ __| '_ \ / _ \/ _ \ __|
+| |___| |___ | |    | | | | | | | | |  __/\__ \ | | |  __/  __/ |_
+ \____|_____|___|   |_| |_|_| |_| |_|\___||___/_| |_|\___|\___|\__|
+
+EOF
+
+printf "		  \e[3mCLI Timesheet by @eetukarttunen\e[0m\n\n"
+
+# Logic for asking current work
+read -p "What are you currently working on? " task
+echo $task
